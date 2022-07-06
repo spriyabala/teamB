fc5a544c374fb12040f7734e254cd51e9ed5ce24
Vaishnnavi <vaishnnavi@Vaishnnavis-MacBook-Pro.local>
2022-07-06 20:28:11 +0530

    Refactored .added test case for main menu

--- src/test/java/com/tw/vapsi/biblioteca/controller/HomeControllerTest.java
+++ src/test/java/com/tw/vapsi/biblioteca/controller/HomeControllerTest.java
@@ -5,10 +5,10 @@ import com.tw.vapsi.biblioteca.controller.helper.ControllerTestHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,9 +26,11 @@ class HomeControllerTest extends ControllerTestHelper {
     }
 
     @Test
+    @WithMockUser()
     void shouldShowMainMenu() throws Exception {
         mockMvc.perform(get("/mainMenu").contentType("application/json"))
-                .andExpect(status().is3xxRedirection());
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("mainMenu"));
 
     }
 }
